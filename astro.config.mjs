2dd5e8c02c7ceed9b0c04a1a91b7a92c175999ea
hsimpson270 <34924402+hsimpson270@users.noreply.github.com>
2025-01-28 14:26:08 -0600

    Added sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hunter-simpson.dev',
+
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [sitemap()],
 });
