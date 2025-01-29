2c3472e2e9dd8b4560c655525bb5280ee3cb8fd7
hsimpson270 <34924402+hsimpson270@users.noreply.github.com>
2025-01-28 22:24:56 -0600

    Updated cloudflare configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import playformInline from '@playform/inline';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hunter-simpson.dev',
@@ -10,5 +12,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [sitemap()],
+  integrations: [sitemap(), playformInline()],
 });
