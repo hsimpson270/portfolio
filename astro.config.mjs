5709e2aaccf377507e0a3fe1df04054fe25fbea5
hsimpson270 <34924402+hsimpson270@users.noreply.github.com>
2025-01-29 11:00:46 -0600

    Converted nav icons to use astro-icon library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 // @ts-check
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
 import playformInline from '@playform/inline';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hunter-simpson.dev',
-
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [sitemap(), playformInline()],
+  integrations: [icon(), playformInline(), sitemap()],
 });
