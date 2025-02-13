f2cd7714b94d76501f9fa5048f275238145f5de6
hsimpson270 <34924402+hsimpson270@users.noreply.github.com>
2025-02-13 14:58:58 -0600

    Added rss feed to site

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss';
+import { getBlogPostsAsync } from '@utils/getBlogPosts';
+
+type Context = {
+  site: string;
+};
+
+export async function GET(context: Context) {
+  const posts = await getBlogPostsAsync();
+
+  return rss({
+    title: "Hunter Simpson's Blog",
+    description: 'A collection of my ramblings.',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      link: `/blog/${post.id}/`,
+      pubDate: post.data.date,
+    })),
+  });
+}
