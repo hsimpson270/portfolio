48697e1736500907d47c1f38881335a28aa2a1d6
hsimpson270 <34924402+hsimpson270@users.noreply.github.com>
2025-02-13 14:06:44 -0600

    Created first blog post

--- src/interfaces/head-props.ts
+++ src/interfaces/head-props.ts
@@ -1,6 +1,14 @@
+import type { OgContentTypes } from '@enums/og-content-types';
+
 export interface HeadProps {
   /** The description of the page shown in search engine results. */
   description: string;
+
+  /**
+   * The og:type value to be set in the header.
+   * Should be either 'article' or 'website' and defaults to 'website'.
+   */
+  ogContent?: OgContentTypes;
 
   /** Link to the image to be shown within open graph links. */
   ogImageUrl?: string;
